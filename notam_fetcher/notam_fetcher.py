bb5a9814375920fe40c91ae7364b955708aaf0f5
Donahue Avila <donahue.avila@gmail.com>
2025-02-13 18:49:55 -0600

    Add fetchNotamsByAirportCode and tests

--- notam_fetcher/notam_fetcher.py
+++ notam_fetcher/notam_fetcher.py
@@ -17,6 +17,10 @@ class NotamFetcherUnexpectedError(NotamFetcherException):
 class NotamFetcherRequestError(NotamFetcherException):
     """Raised when NotamFetcher receives a request exception while fetching from the API"""
 
+class NotamFetcherUnauthenticated(NotamFetcherException):
+    """Raised when cliend_id or client_secret are invalid"""
+
+
 class NotamFetcher:
     DOMAIN = "https://external-api.faa.gov/notamapi/v1/notams"
 
@@ -25,6 +29,31 @@ class NotamFetcher:
         self.client_secret = client_secret
         self._pageSize = 50
 
+    def fetchNotamsByAirportcode(self, airportCode: str):
+        """
+        Fetches ALL notams for a particular latitude and longitude.
+
+        Args:
+            airportCode (str): A valid airport code.
+        
+        Raises:
+            NotamFetcherRequestError: If a request error occurs while fetching from the API.
+            NotamFetcherUnexpectedError: If an unexpected error occurs.
+        Returns:
+            Notams (List[dict[str, Any]]): A list of NOTAM dicts
+        """
+        notamItems: list[dict[str, Any]] = []
+
+        first_page = self._fetchNotamsByAirportCode(airportCode, 1, self._pageSize)
+        totalPages = first_page['totalPages']
+
+        notamItems.extend(first_page['items'])
+
+        for i in range(2, totalPages+1):
+            nextPage = self._fetchNotamsByAirportCode(airportCode, i, self._pageSize)
+            notamItems.extend(nextPage['items'])
+        return notamItems
+    
     def fetchNotamsByLatLong(self, lat: float, long: float, radius: float = 100.0):
         """
         Fetches ALL notams for a particular latitude and longitude.
@@ -40,8 +69,6 @@ class NotamFetcher:
         Returns:
             Notams (List[dict[str, Any]]): A list of NOTAM dicts
         """
-
-        
         notamItems: list[dict[str, Any]] = []
 
         first_page = self._fetchNotamsByLatLong(lat, long, radius, 1, self._pageSize)
@@ -94,6 +121,53 @@ class NotamFetcher:
             raise NotamFetcherRequestError from e
 
         try: 
-            return response.json()
+            data = response.json()
+            if data.get("error", "") == "Invalid client id or secret":
+                raise(NotamFetcherUnauthenticated("Invalid client id or secret"))
+            
+            return data
+        except requests.exceptions.JSONDecodeError:
+            raise(NotamFetcherUnexpectedError(f"Response from API unexpectedly not JSON. Received text: {response.text} "))
+
+
+    def _fetchNotamsByAirportCode(self, airportCode: str, pageNum: int , pageSize: int=1000) -> NotamResponseDict:
+        """
+        Fetches a response from the API using latitude and longitude.
+
+        Args:
+            airportCode (str): A valid airport code.
+            pageNum (int): The page number of the response (min: 1)
+            pageSize (int): The number of NOTAMs per page (max: 1000)
+
+        Returns:
+            dict: JSON response containing NOTAM data
+
+        Raises:
+            NotamFetcherRequestError: If a request error occurs while fetching from the API.
+            NotamFetcherUnexpectedError: If an unexpected error occurs.
+        """
+
+        querystring = {
+            "domesticLocation": str(airportCode),
+            "pageNum": str(pageNum),
+            "pageSize": str(pageSize)
+        }
+
+        try:
+            response = requests.get(
+                self.DOMAIN,
+                headers={"client_id": self.client_id, "client_secret": self.client_secret},
+                params=querystring,
+            )
+
+        except requests.exceptions.RequestException as e:
+            raise NotamFetcherRequestError from e
+
+        try: 
+            data = response.json()
+            if data.get("error", "") == "Invalid client id or secret":
+                raise(NotamFetcherUnauthenticated("Invalid client id or secret"))
+            
+            return data
         except requests.exceptions.JSONDecodeError:
             raise(NotamFetcherUnexpectedError(f"Response from API unexpectedly not JSON. Received text: {response.text} "))
--- tests/test_notam_fetcher.py
+++ tests/test_notam_fetcher.py
@@ -3,13 +3,13 @@ from pytest import MonkeyPatch
 import pytest
 from notam_fetcher.notam_fetcher import NotamFetcher, NotamResponseDict
 
-
+from typing import Any
 
 
 @pytest.fixture
 def mock_empty_response(monkeypatch: MonkeyPatch):
 
-    def returnEmpty(self: NotamFetcher, lat: float, long: float, radius: float, pageNum: int , pageSize: int=1000) -> NotamResponseDict:
+    def returnEmpty(*args: Any) -> NotamResponseDict:
         return {
                 "pageSize": 50,
                 "pageNum": 1,
@@ -19,8 +19,14 @@ def mock_empty_response(monkeypatch: MonkeyPatch):
         }
     
     monkeypatch.setattr(NotamFetcher, "_fetchNotamsByLatLong", returnEmpty)
+    monkeypatch.setattr(NotamFetcher, "_fetchNotamsByAirportCode", returnEmpty)
 
 def test_fetchNotamsByLatLong_no_notams(mock_empty_response: None):
     notam_fetcher = NotamFetcher("CLIENT_ID", "CLIENT_SECRET")
     notams = notam_fetcher.fetchNotamsByLatLong(32, 32, 10)
+    assert(len(notams) == 0)
+
+def test_fetchNotamsByAirportCode_no_notams(mock_empty_response: None):
+    notam_fetcher = NotamFetcher("CLIENT_ID", "CLIENT_SECRET")
+    notams = notam_fetcher.fetchNotamsByAirportcode("LAX")
     assert(len(notams) == 0)
